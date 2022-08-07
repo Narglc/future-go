104b3445bb9bcc6d3d63ae2bd711fa58b1a0a00b
Narglc <qingcheliuzhi@qq.com>
2022-08-07 21:26:28 +0800

    增加其他方式的路由

--- gin_demo/gin_stupid/router/router.go
+++ gin_demo/gin_stupid/router/router.go
@@ -6,12 +6,49 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/* Gin 的路由支持 GET , POST , PUT , DELETE , PATCH , HEAD , OPTIONS 请求，
+同时还有一个 Any 函数，可以同时支持以上的所有请求。
+*/
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	// 添加Get请求路由
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello gin")
-	})
+	{
+		// 添加Get请求路由
+		r.GET("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello gin get method")
+		})
+
+		// 添加Post请求路由
+		r.POST("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello gin post method")
+		})
+
+		// 添加Put请求路由
+		r.PUT("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello gin put method")
+		})
+
+		// 添加Delete请求路由
+		r.DELETE("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello gin delete method")
+		})
+
+		// 添加Patch请求路由
+		r.PATCH("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello gin patch method")
+		})
+
+		// 添加Head请求路由
+		r.HEAD("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello gin head method")
+		})
+
+		// 添加Options请求路由
+		r.OPTIONS("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello gin options method")
+		})
+
+	}
+
 	return r
 }
--- gin_demo/gin_stupid/router/router_test.go
+++ gin_demo/gin_stupid/router/router_test.go
@@ -15,5 +15,16 @@ func TestRouter(t *testing.T) {
 	req, _ := http.NewRequest(http.MethodGet, "/", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "hello gin", w.Body.String())
+	assert.Equal(t, "hello gin get method", w.Body.String())
+}
+
+// router("/") post 测试
+func TestIndexPostRouter(t *testing.T) {
+	router := SetupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "hello gin post method", w.Body.String())
 }
