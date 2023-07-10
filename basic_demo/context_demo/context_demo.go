eee1d9ac5556d010f6fe8598d906b99501cc31ca
Narglc Yu <qingcheliuzhi@qq.com>
2023-07-10 17:46:22 +0800

    context的使用

---
+++ basic_demo/context_demo/context_demo.go
@@ -0,0 +1,72 @@
+package contextdemo
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func value() {
+	// 初识ctx使用Backgroud、TODO都可以
+	ctx := context.Background() // context.TODO()
+
+	// 添加key-value键值对
+	ctx = context.WithValue(ctx, "Name", "Narglc")
+	ctx = context.WithValue(ctx, "Age", 18)
+	ctx = context.WithValue(ctx, "Addr", "Shenzhen Nanshan Xili")
+
+	keys := []string{"Name", "Age", "Addr"}
+
+	for idx, key := range keys {
+		fmt.Println(idx, ":", key, " --> ", ctx.Value(key))
+	}
+}
+
+// context.WithTimeout, ctx.Done(), ctx.Err()
+func timeout() {
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*3) //time.Millisecond * 1000
+	t0 := time.Now()
+	defer cancel()
+
+	select {
+	case <-ctx.Done(): // ctx 被取消时，则返回
+
+		// 使用 time.Since 计算耗时
+		fmt.Println("time.Since:", time.Since(t0).Milliseconds())
+		err := ctx.Err()
+		fmt.Println("ctx err:", err)
+	}
+}
+
+func cancel() {
+	ctx, cancel := context.WithCancel(context.TODO())
+	t0 := time.Now()
+
+	go func() {
+		time.Sleep(time.Millisecond * 100)
+		cancel()
+	}()
+
+	select {
+	case <-ctx.Done():
+		t1 := time.Now()
+		// 使用 time.Sub 计算耗时
+		fmt.Println("time.Sub:", t1.Sub(t0).Milliseconds())
+		err := ctx.Err()
+		fmt.Println(err)
+	}
+}
+
+func TestContext() {
+	// 计算函数耗时
+	t0 := time.Now()
+	defer func() {
+		fmt.Printf("cur func Cost %d ms\n", time.Since(t0).Milliseconds())
+	}()
+
+	fmt.Println("TestContext:hello world  ==============")
+
+	value()
+	timeout()
+	cancel()
+}
--- basic_demo/main.go
+++ basic_demo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	contextdemo "future-go/basic_demo/context_demo"
 	flagdemo "future-go/basic_demo/flag_demo"
 	_ "future-go/basic_demo/init_demo"       // 匿名引入包，只为在main之前调用init_demo中的init函数进行资源初始化
 	demo "future-go/basic_demo/package_demo" // 包的路径: 以goPath的src为根目录定义,仅到目标目录为止,且必须双引号
@@ -16,4 +17,7 @@ func main() {
 
 	/* ./main.exe -u narglc -p 9527123 -h 192.254.1.16 -port 1024 */
 	flagdemo.InitFlag()
+
+	/* context demo */
+	contextdemo.TestContext()
 }
