e5f8a4f135972f8db16dc0ca40121a9cb921adff
Narglc Yu <qingcheliuzhi@qq.com>
2024-07-02 19:39:10 +0800

    micro_mall:增加UserRpc的配置

--- go-zero_demo/micro_mall/order/internal/svc/servicecontext.go
+++ go-zero_demo/micro_mall/order/internal/svc/servicecontext.go
@@ -2,14 +2,19 @@ package svc
 
 import (
 	"future-go/go-zero_demo/micro_mall/order/internal/config"
+	"future-go/go-zero_demo/micro_mall/user/userclient"
+
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config  config.Config
+	UserRpc userclient.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:  c,
+		UserRpc: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
