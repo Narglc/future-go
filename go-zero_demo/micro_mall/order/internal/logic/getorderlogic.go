c37739c794dd372df2f130c951bd25683c805aff
Narglc Yu <qingcheliuzhi@qq.com>
2024-07-02 19:57:01 +0800

    micro_mall:完善rpc调用实现

--- go-zero_demo/micro_mall/order/internal/logic/getorderlogic.go
+++ go-zero_demo/micro_mall/order/internal/logic/getorderlogic.go
@@ -5,6 +5,7 @@ import (
 
 	"future-go/go-zero_demo/micro_mall/order/internal/svc"
 	"future-go/go-zero_demo/micro_mall/order/internal/types"
+	"future-go/go-zero_demo/micro_mall/user/types/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -25,6 +26,23 @@ func NewGetOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetOrder
 
 func (l *GetOrderLogic) GetOrder(req *types.OrderReq) (resp *types.OrderReply, err error) {
 	// todo: add your logic here and delete this line
+
+	userId := req.Id
+
+	// 根据用户id去user服务获取用户信息
+	userResponse, err := l.svcCtx.UserRpc.GetUser(context.Background(), &user.IdRequest{
+		Id: userId,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp = &types.OrderReply{
+		Id:       userResponse.Id,
+		Name:     "hello user name.",
+		UserName: userResponse.Name,
+	}
 
 	return
 }
